c9f33049c25a82fc0d8aa0dec07a0b0c2fd55a4f
0x62 <ben@ovalbit.com>
2022-01-24 21:51:35 +0000

    Latest

--- IssueGenerator.js
+++ IssueGenerator.js
@@ -78,7 +78,7 @@ export default class IssueGenerator extends EventEmitter {
     this.issue = new Issue({ updateOnly: true })
     this.providers = [messages]
 
-    const blocks = [new Blocks.Spacer(40)]
+    const blocks = [new Blocks.Spacer(20)]
     const { newMessages } = messages.data
 
     newMessages.forEach((message, idx) => {
@@ -87,7 +87,7 @@ export default class IssueGenerator extends EventEmitter {
       } else {
         blocks.push(new Blocks.Message(message))
       }
-      blocks.push(new Blocks.Spacer(40))
+      blocks.push(new Blocks.Spacer(80))
     })
 
     this.issue.addBlocks(...blocks)
