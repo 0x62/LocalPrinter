3b07d10a8593ac4bcd1aafd81a123e6565f93cb8
0x62 <ben@ovalbit.com>
2022-02-09 20:56:24 +0000

    Latest

--- plugins/news/blocks/LeadPhoto.js
+++ plugins/news/blocks/LeadPhoto.js
@@ -8,31 +8,30 @@ export default class BlockNewsLeadPhoto extends Block {
     this.url = url
   }
 
+   // Set the canvas and startPosY (called by IssueGenerator)
+  _setupCanvas(issue) {
+    // this.issue = issue
+    // this.canvas = issue.canvas
+    // this.ctx = issue.ctx
+    // this.startPosY = issue.height
+    this.issue = issue
+    this.canvas = createCanvas(500, 250)
+    this.ctx = this.canvas.getContext('2d')
+    this.startPosY = 0
+  }
+
   // Render the current block to the canvas with
   // this.canvas, this.ctx, this.startPosY
   async render() {
     const image = await this._loadRemoteImage(this.url)
     const WIDTH = 500, HEIGHT = 250
 
-    console.log('DEBUG [LeadPhoto] creating new canvas')
-
-    // Create a new canvas so we can center the image and crop to square
-    const slot = createCanvas(WIDTH, HEIGHT)
-    const slotCtx = slot.getContext('2d')
     const hRatio = WIDTH / image.width
     const vRatio = HEIGHT / image.height
     const ratio = Math.max(hRatio, vRatio)
     const shiftX = (WIDTH - image.width*ratio) / 2
     const shiftY = (HEIGHT - image.height*ratio) / 2
-    slotCtx.drawImage(image, 0, 0, image.width, image.height, shiftX, shiftY, image.width * ratio, image.height * ratio)
-
-    console.log('DEBUG [LeadPhoto] image drawn')
-
-    const imgData = slotCtx.getImageData(0, 0, WIDTH, HEIGHT)
-    console.log('DEBUG [LeadPhoto] got image data')
-    this._drawDitheredData(imgData, 0, this.startPosY)
-
-    console.log('DEBUG [LeadPhoto] drew dithered image')
+    this.ctx.drawImage(image, 0, 0, image.width, image.height, shiftX, shiftY, image.width * ratio, image.height * ratio)
 
     return { endPosY: this.startPosY + HEIGHT }
   }
